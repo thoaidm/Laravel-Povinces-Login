cee0a93e666c63d632cc2062f5607e12a54b5e62
lilzom <khuudunguyen@gmail.com>
2021-07-29 13:46:53 +0700

    2nd commit

--- resources/views/create.blade.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="{{route('store')}}">
-    @csrf
-    name: <input type="text" name="name">
-    created_at: <input hidden type="text" name="created_at">
-    updated_at: <input hidden type="text" name="updated_at">
-    deleted_at: <input hidden type="text" name="deleted_at">
-    <button type="submit">Save</button>
-</form>
--- resources/views/partials/footer-script.blade.php
+++
@@ -1,7 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jszip-2.5.0/dt-1.10.25/af-2.3.7/b-1.7.1/b-colvis-1.7.1/b-html5-1.7.1/b-print-1.7.1/cr-1.5.4/date-1.1.0/fc-3.3.3/fh-3.1.9/kt-2.6.2/r-2.2.9/rg-1.1.3/rr-1.2.8/sc-2.0.4/sb-1.1.0/sp-1.3.0/sl-1.3.3/datatables.min.js"></script>
